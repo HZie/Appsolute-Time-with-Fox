c20d121b3c4544306b3d970c2d9f6d0385654954
siyeonkim <ashley_0726@naver.com>
2020-08-30 01:29:50 +0900

    월요일 접속시 여우와 작별하는 이벤트창 추가하였습니다!(오아시스에 추가됐느냐 안됐느냐를 좀더 자세히 보여주기위해서)

--- app/src/main/java/com/example/todolist/OasisFragment.java
+++ app/src/main/java/com/example/todolist/OasisFragment.java
@@ -18,17 +18,17 @@ import com.example.todolist.databinding.FragmentTodoBinding;
 // TODO: 오아시스(놀이방)관련 코드
 
 public class OasisFragment extends Fragment {
-    ImageButton fNormal; ImageButton fSwim; ImageButton fSleep; ImageButton fRed; ImageButton fPilot;
-    ImageButton fAngry;
+    ImageButton fNormal; ImageButton fSwim; ImageButton fSleep; ImageButton fBaby; ImageButton fPilot;
+    ImageButton fAngry; ImageButton fLife;
+
+    Boolean[] farr = new Boolean[7];
 
     FragmentOasisBinding binding;
 
     int imageResources[] = {
-            R.drawable.fox_swim, R.drawable.fox, R.drawable.fox_sleep, R.drawable.fox_red, R.drawable.fox_pilot,
-            R.drawable.fox_angry,
+            R.drawable.fox_swim, R.drawable.fox, R.drawable.fox_sleep, R.drawable.fox_pilot, R.drawable.fox_angry,
+            R.drawable.fox_baby,  R.drawable.fox_lifeguard,
     };
-
-    Boolean[] farr = new Boolean[imageResources.length];
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -43,8 +43,9 @@ public class OasisFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         fNormal =view.findViewById(R.id.fNormal); fSwim =view.findViewById(R.id.fSwim);
-        fSleep =view.findViewById(R.id.fSleep); fRed =view.findViewById(R.id.fRed);
+        fSleep =view.findViewById(R.id.fSleep); fBaby =view.findViewById(R.id.fBabay);
         fPilot =view.findViewById(R.id.fPilot); fAngry =view.findViewById(R.id.fAngry);
+        fLife =view.findViewById(R.id.fLifeg);
 
         farr = ((MainActivity)MainActivity.mcontext).getFoxLog();
         for(int i=0; i<imageResources.length; i++) {
@@ -53,26 +54,12 @@ public class OasisFragment extends Fragment {
                 if (i == 0) fSwim.setVisibility(View.VISIBLE);
                 if (i == 1) fNormal.setVisibility(View.VISIBLE);
                 if (i == 2) fSleep.setVisibility(View.VISIBLE);
-                if (i == 3) fRed.setVisibility(View.VISIBLE);
-                if (i == 4) fPilot.setVisibility(View.VISIBLE);
+                if (i == 3) fPilot.setVisibility(View.VISIBLE);
+                if (i == 4) fBaby.setVisibility(View.VISIBLE);
                 if (i == 5) fAngry.setVisibility(View.VISIBLE);
+                if (i == 6) fLife.setVisibility(View.VISIBLE);
             }
         }
-        float oasisNum = (float)1332/(float)986;
-        float oasisNum2 = (float)986/(float)1332;
-        float X = ((MainActivity)MainActivity.mcontext).pointX;
-        float Y = ((MainActivity)MainActivity.mcontext).pointY;
-        float ground_width; float ground_height;
-        ImageView image = view.findViewById(R.id.oasis_ground);
-
-        if(Y/X < oasisNum) {
-            ground_width  = Y*oasisNum2;
-        }
-        else ground_height = X*oasisNum;
-
-
-
-
     }
 
 }
